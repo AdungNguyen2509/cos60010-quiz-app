c3eda9f02c57c810484ada5b9f9fd06e8d34c919
AdungNguyen2509 <adungg2509@gmail.com>
2022-05-01 16:41:19 +1000

    login api & password hashsing

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+//hashing library
+const bcrypt = require("bcrypt")
+const saltRounds = 10
+
 const User = require("./model/User")
+
 const PORT = process.env.PORT || 3000
 
 //Connect to mongo atlas
@@ -20,28 +25,67 @@ app.route("/")
 })
 
 .post((req,res)=> {
+    //Username and password data
     const username = req.body.username
     const password = req.body.password
+    //hashing password function
+    const hash = bcrypt.hashSync(password, saltRounds)
+    //score to add from front end ?
+    const score = req.body.score
 
+    //New user is made and added to mongoDB
     const newUser = new User({
         username: username,
-        password : password
+        //password is hash before adding to mongoDB
+        password : hash,
+        score: score,
+
     }) 
     newUser
     .save()
     .catch((err)=> console.log(err))
+    //catch if user data isnt satisfied data model (model/User.js)
     console.log(newUser)
 
-
-
-
-    
     if (res.statusCode === 200) {
         res.send("successfully added to database")
     }
     else {
         res.send("failed to added to database")
     }
+})
+
+//Login Api route
+app.route("/login")
+.get((req,res) => {
+    res.sendFile(__dirname + "/login.html")
+})
+
+.post((req,res)=> {
+    //Data from login
+    const loginUsername = req.body.username
+    const loginPassword = req.body.password
+    const hash = bcrypt.hashSync(loginPassword, saltRounds)
+
+    //Find username in MongoDB 
+    User.findOne( {username: loginUsername}, function(err, foundUser) {
+        if (err) {
+            console.log(err)
+        }
+        else if (foundUser === null) {
+            res.send("There is no user in database")
+        }
+        else {
+            console.log(foundUser)
+            //matching hashing password
+            if (bcrypt.compareSync(loginPassword, foundUser.password))
+            {
+                res.send("Logged in")
+            }
+        }
+    })
+
+
 })
 
 app.listen(PORT , (req,res)=> {
